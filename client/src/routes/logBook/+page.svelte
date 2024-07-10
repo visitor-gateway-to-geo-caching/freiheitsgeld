4c98add94949c31c5720d45c7de9c6e4fe9e672d
Monique Baumann <monique@freedomcash.org>
2024-07-10 13:22:03 +0200

    free the world

--- client/src/routes/logBook/+page.svelte
+++ client/src/routes/logBook/+page.svelte
@@ -1,31 +1,51 @@
 <script>
-	import LogBook from '$components/LogBook.svelte';
-	import { onMount } from 'svelte';
+	// import { onMount } from 'svelte';
 
-	let visitorHasBrowserWallet = false;
-	let visitorWithMobile = false;
+	// let visitorHasBrowserWallet = false;
+	// let visitorWithMobile = false;
 
-	onMount(async () => {
-		if (typeof window.ethereum === 'undefined') {
-			visitorHasBrowserWallet = false;
-		} else {
-			visitorHasBrowserWallet = true;
-		}
-		if (
-			/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
-		) {
-			visitorWithMobile = true;
-		}
-	});
+	// onMount(async () => {
+	// 	if (typeof window.ethereum === 'undefined') {
+	// 		visitorHasBrowserWallet = false;
+	// 	} else {
+	// 		visitorHasBrowserWallet = true;
+	// 	}
+	// 	if (
+	// 		/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+	// 	) {
+	// 		visitorWithMobile = true;
+	// 	}
+	// });
 </script>
 
 <div class="text-center content">
 	<h2>Spuren der Freiheit</h2>
-	<LogBook {visitorWithMobile}></LogBook>
+	<p><br /></p>
+	Jeder Geo Cacher ist eingeladen Einträge im
+	<a href="https://freedomlog.de" target="_blank">Logbuch der Freiheit</a>
+	zu machen.
+	<p><br /></p>
+	Die Einträge werden auf der
+	<a
+		href="https://polygonscan.com/token/0xb841a4f979f9510760ecf60512e038656e68f459"
+		target="_blank"
+	>
+		Polygon POS Blockchain
+	</a>
+	gespeichert.
+	<p><br /></p>
+	Mit jedem Eintrag kann
+	<a
+		href="https://www.geckoterminal.com/polygon_pos/pools/0x0296f6ea1f02083b99ae59200d00f5257473f17c"
+		target="_blank"
+	>
+		Geo Cash
+	</a>
+	an andere gespendet werden.
 
-	{#if !visitorHasBrowserWallet}
-	 	<p><br><br><br></p>
-	{/if}
+	<p><br /><br /></p>
+
+	<!-- <LogBook {visitorWithMobile}></LogBook> -->
 </div>
 
 <style>
---
+++ docs/app/immutable/nodes/5.5b6404ff.js
@@ -0,0 +1,14 @@
+import{s as o,n as a}from"../chunks/scheduler.46233b48.js";import{S as i,i as l,g as p,h as c,z as h,k as g,a as d,f}from"../chunks/index.a26073d0.js";function m(r){let e,n=`<h2>Spuren der Freiheit</h2> <p><br/></p>
+	Jeder Geo Cacher ist eingeladen Einträge im
+	<a href="https://freedomlog.de" target="_blank">Logbuch der Freiheit</a>
+	zu machen.
+	<p><br/></p>
+	Die Einträge werden auf der
+	<a href="https://polygonscan.com/token/0xb841a4f979f9510760ecf60512e038656e68f459" target="_blank">Polygon POS Blockchain</a>
+	gespeichert.
+	<p><br/></p>
+	Mit jedem Eintrag kann
+	<a href="https://www.geckoterminal.com/polygon_pos/pools/0x0296f6ea1f02083b99ae59200d00f5257473f17c" target="_blank">Geo Cash</a>
+	an andere gespendet werden.
+
+	<p><br/><br/></p>`;return{c(){e=p("div"),e.innerHTML=n,this.h()},l(t){e=c(t,"DIV",{class:!0,"data-svelte-h":!0}),h(e)!=="svelte-vpjrn8"&&(e.innerHTML=n),this.h()},h(){g(e,"class","text-center content svelte-gu7t45")},m(t,s){d(t,e,s)},p:a,i:a,o:a,d(t){t&&f(e)}}}class b extends i{constructor(e){super(),l(this,e,null,m,o,{})}}export{b as component};
